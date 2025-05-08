b84dc3acfba480d5432ff31ccac932d5ac0b607a
FedorenkoCodes <3668729+FedorenkoCodes@users.noreply.github.com>
2025-05-09 00:08:57 +0200

    Wow, nice refactoring

--- calc.rb
+++ calc.rb
@@ -1,47 +1,51 @@
 require 'json'
 
-def get_user_input
-  origin_port = nil
-  destination_port = nil
-  criteria = nil
+class ShippingCalculator
+  def initialize
+    @origin_port = nil
+    @destination_port = nil
+    @criteria = nil
+  end
 
-  while (line = gets)
-    line = line.chomp
+  def calculate
+    get_user_input
 
-    if origin_port.nil?
-      origin_port = line.strip
-    elsif destination_port.nil?
-      destination_port = line.strip
-    elsif criteria.nil?
-      criteria = line.strip
+    if input_valid?(@origin_port, @destination_port, @criteria)
+      output_data = [
+        {
+          origin_port: @origin_port,
+          destination_port: @destination_port,
+          criteria: @criteria,
+        }
+      ]
+      output_json = JSON.pretty_generate(output_data)
+      puts output_json
+    else
+      puts "Invalid input"
     end
   end
 
-  return [origin_port, destination_port, criteria]
+  def get_user_input
+    while (line = gets)
+      line = line.chomp
+
+      if @origin_port.nil?
+        @origin_port = line.strip
+      elsif @destination_port.nil?
+        @destination_port = line.strip
+      elsif @criteria.nil?
+        @criteria = line.strip
+      end
+    end
+  end
+
+  def input_valid?(origin_port, destination_port, criteria)
+    return false unless origin_port && destination_port && criteria
+    return false if origin_port.length >= 100 || destination_port.length >= 100 || criteria.length >= 100
+    return false unless ['cheapest-direct', 'cheapest', 'fastest'].include?(criteria)
+
+    true
+  end
 end
 
-def input_valid?(origin_port, destination_port, criteria)
-  return false unless origin_port && destination_port && criteria
-  return false if origin_port.length >= 100 || destination_port.length >= 100 || criteria.length >= 100
-  return false unless ['cheapest-direct', 'cheapest', 'fastest'].include?(criteria)
-
-  true
-end
-
-origin_port, destination_port, criteria = get_user_input
-
-if input_valid?(origin_port, destination_port, criteria)
-  output_data = [
-    {
-      origin_port: origin_port,
-      destination_port: destination_port,
-      criteria: criteria,
-    }
-  ]
-  output_json = JSON.pretty_generate(output_data)
-  puts output_json
-else
-  puts "Invalid input"
-end
-
-exit
+ShippingCalculator.new.calculate
