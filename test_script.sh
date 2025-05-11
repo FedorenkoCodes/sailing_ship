1400d7e132b30b90a93832775cb0872fbbfcddf9
FedorenkoCodes <3668729+FedorenkoCodes@users.noreply.github.com>
2025-05-11 12:44:47 +0200

    Let's TDD

--- test_script.sh
+++ test_script.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
 super_duper_function_to_run_the_test() {
-  local actual_output="$1"
-  local expected_output="$2"
+  actual_output="$1"
+  expected_output="$2"
 
-  if [ "$output" == "$expected_output" ]; then
+  if [ "$output" = "$expected_output" ]; then
     echo "Test passed"
   else
     echo "Test failed"
 
     echo "Expected output:"
-    echo $expected_output
+    echo "$expected_output"
 
     echo "Actual output:"
-    echo $actual_output
+    echo "$actual_output"
   fi
 }
 
@@ -34,25 +34,51 @@ expected_output='[
 ]'
 super_duper_function_to_run_the_test "$output" "$expected_output"
 
-# Test for "qwe \n rty \n fastest \n"
-output=$(printf "qwe \n rty \n fastest \n" | docker run -i --rm calculator)
+# Test for "CNSHA \n NLRTM \n fastest \n"
+output=$(printf "CNSHA \n NLRTM \n fastest \n" | docker run -i --rm calculator)
 expected_output='[
   {
-    "origin_port": "qwe",
-    "destination_port": "rty",
-    "criteria": "fastest"
+    "origin_port": "CNSHA",
+    "destination_port": "ESBCN",
+    "departure_date": "2022-01-29",
+    "arrival_date": "2022-02-12",
+    "sailing_code": "ERXQ",
+    "rate": "261.96",
+    "rate_currency": "EUR"
+  },
+  {
+    "origin_port": "ESBCN",
+    "destination_port": "NLRTM",
+    "departure_date": "2022-02-16",
+    "arrival_date": "2022-02-20",
+    "sailing_code": "ETRG",
+    "rate": "69.96",
+    "rate_currency": "USD"
   }
 ]'
 
 super_duper_function_to_run_the_test "$output" "$expected_output"
 
-# Test for "qwe \n rty \n cheapest \n"
-output=$(printf "qwe \n rty \n cheapest \n" | docker run -i --rm calculator)
+# Test for "CNSHA \n NLRTM \n cheapest \n"
+output=$(printf "CNSHA \n NLRTM \n cheapest \n" | docker run -i --rm calculator)
 expected_output='[
   {
-    "origin_port": "qwe",
-    "destination_port": "rty",
-    "criteria": "cheapest"
+    "origin_port": "CNSHA",
+    "destination_port": "ESBCN",
+    "departure_date": "2022-01-29",
+    "arrival_date": "2022-02-12",
+    "sailing_code": "ERXQ",
+    "rate": "261.96",
+    "rate_currency": "EUR"
+  },
+  {
+    "origin_port": "ESBCN",
+    "destination_port": "NLRTM",
+    "departure_date": "2022-02-16",
+    "arrival_date": "2022-02-20",
+    "sailing_code": "ETRG",
+    "rate": "69.96",
+    "rate_currency": "USD"
   }
 ]'
 super_duper_function_to_run_the_test "$output" "$expected_output"
