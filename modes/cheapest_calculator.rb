bfd46b7592b2a2e30cfdaf4116f84a733d0fbabd
FedorenkoCodes <3668729+FedorenkoCodes@users.noreply.github.com>
2025-05-11 14:18:52 +0200

    Add calculation of the cheapest indirect sailing

--- modes/cheapest_calculator.rb
+++ modes/cheapest_calculator.rb
@@ -6,6 +6,24 @@ class CheapestCalculator
     rates = data['rates']
     exchange_rates = data['exchange_rates']
 
-    [sailings.first]
+    filtered_sailings = sailings.select do |sailing|
+      sailing['origin_port'] == origin_port && sailing['destination_port'] == destination_port
+    end
+
+    raise "No sailings found between #{origin_port} and #{destination_port}" if filtered_sailings.empty?
+
+    filtered_sailings.each do |sailing|
+      rate = rates.find { |r| r['sailing_code'] == sailing['sailing_code'] }
+
+      raise "No rate found for sailing #{sailing['sailing_code']}" if rate.nil?
+
+      sailing['rate'] = rate['rate']
+      sailing['rate_currency'] = rate['rate_currency']
+      sailing['converted_rate'] = CurrencyConverter.convert_to_eur(rate['rate'], rate['rate_currency'], sailing['departure_date'], exchange_rates)
+    end
+
+    cheapest_sailing = filtered_sailings.min_by { |s| s['converted_rate'].to_f }
+
+    [cheapest_sailing]
   end
 end
